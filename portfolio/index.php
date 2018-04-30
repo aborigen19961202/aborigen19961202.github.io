6eed8a1e585df4dae09600c543cae7923c9718a7
Aborigen19961202 <aborigen19961202@gmail.com>
2018-04-30 15:42:29 +0300

    add
    add

---
+++ portfolio/index.php
@@ -0,0 +1,367 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Головна</title>
+		<meta charset="utf-8">
+		<meta name="apple-mobile-web-app-capable" content="yes">
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+		<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+		<link rel="stylesheet" href="css/lightslider.css">
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.10/css/all.css" integrity="sha384-+d0P83n9kaQMCwj8F4RJB66tzIwOKmrdb46+porD/OvrJ+37WqIM7UoBtwHO6Nlg" crossorigin="anonymous">
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+		<script src="https://unpkg.com/scrollreveal/dist/scrollreveal.min.js"></script>
+		<meta http-equiv="Cache-Control" content="no-cache">
+	</head>
+	<body>
+		<div class="header">
+			<div class="header__top">
+				<div class="animate-logo">
+					<svg xmlns="http://www.w3.org/2000/svg" width="350" height="300" viewBox="0 0 700 600">
+						<path id="path5419" d="m 627.55727,563.46269 -34.09265,-47.72968 39.14342,27.7792 z"/>
+						<path id="path4232" d="m 602.30346,512.19745 -8.83884,3.53556 39.14342,27.7792 z"/>
+						<path id="path4236" d="m 602.30346,512.19745 -8.83884,3.53556 -1.26268,-35.35533 z"/>
+						<path id="path4240" d="m 564.92782,487.19618 28.5368,28.53683 -1.26268,-35.35533 z"/>
+						<path id="path4244" d="m 564.92782,487.19618 11.11167,-17.42511 16.16245,10.60661 z"/>
+						<path id="path4248" d="m 564.92782,487.19618 11.11167,-17.42511 -22.72842,-7.3236 z"/>
+						<path id="path4252" d="m 561.13975,454.11368 14.89974,15.65739 -22.72842,-7.3236 z"/>
+						<path id="path4628" d="m 561.13975,454.11368 -6.17169,-8.62832 -1.65699,16.96211 z"/>
+						<path id="path4632" d="m 538.28261,426.25654 16.68545,19.22882 -1.65699,16.96211 z"/>
+						<path id="path4634" d="m 564.92782,487.19618 -70.67404,-28.1394 59.05729,3.39069 z"/>
+						<path id="path4636" d="m 538.49925,425.41047 -44.24547,33.64631 59.05729,3.39069 z"/>
+						<path id="path4644" d="m 538.49925,425.41047 -44.24547,33.64631 15.843,-45.89502 z"/>
+						<path id="path4646" d="m 435.99925,426.83904 58.25453,32.21774 15.843,-45.89502 z"/>
+						<path id="path4648" d="m 435.99925,426.83904 60.75453,-44.56797 13.343,30.89069 z"/>
+						<path id="path4656" d="m 435.99925,426.83904 60.75453,-44.56797 -74.51414,21.60498 z"/>
+						<path id="path4658" d="m 452.42782,338.9819 44.32596,43.28917 -74.51414,21.60498 z"/>
+						<path id="path4660" d="m 452.42782,338.9819 44.32596,43.28917 -10.94271,-34.82359 z"/>
+						<path id="path4662" d="m 511.71353,386.12476 -14.95975,-3.85369 -10.94271,-34.82359 z"/>
+						<path id="path4672" d="m 452.42782,338.9819 -61.38833,0.43203 31.20015,64.46212 z"/>
+						<path id="path4674" d="m 376.71353,406.83904 14.32596,-67.42511 31.20015,64.46212 z"/>
+						<path id="path4676" d="m 376.71353,406.83904 -7.10261,29.00346 52.62872,-31.96645 z"/>
+						<path id="path4678" d="m 435.28496,425.41047 -65.67404,10.43203 52.62872,-31.96645 z"/>
+						<path id="path4688" d="m 376.71353,406.83904 14.32596,-67.42511 -25.22842,-11.25217 z"/>
+						<path id="path4690" d="m 376.71353,406.83904 -52.10261,-4.56797 41.20015,-74.10931 z"/>
+						<path id="path4692" d="m 376.71353,406.83904 -52.10261,-4.56797 44.05729,34.46212 z"/>
+						<path id="path4694" d="m 313.85639,422.55333 10.75453,-20.28226 44.05729,34.46212 z"/>
+						<path id="path4704" d="m 318.1421,328.9819 6.46882,73.28917 41.20015,-74.10931 z"/>
+						<path id="path4706" d="m 318.1421,328.9819 6.46882,73.28917 -48.08556,-20.53788 z"/>
+						<path id="path4708" d="m 313.1421,422.55333 11.46882,-20.28226 -48.08556,-20.53788 z"/>
+						<path id="path4729" d="m 313.1421,422.55333 -48.53118,-12.42512 11.91444,-28.39502 z"/>
+						<path id="path4731" d="m 318.1421,328.9819 -68.53118,-38.85369 26.91444,91.60498 z"/>
+						<path id="path4733" d="m 215.99924,358.26761 33.61168,-68.1394 26.91444,91.60498 z"/>
+						<path id="path4735" d="m 215.99924,358.26761 -2.81689,21.8606 63.34301,1.60498 z"/>
+						<path id="path4743" d="m 264.57067,410.41047 -51.38832,-30.28226 63.34301,1.60498 z"/>
+						<path id="path4747" d="m 264.57067,410.41047 -51.38832,-30.28226 1.20015,17.31927 z"/>
+						<path id="path4749" d="m 264.57067,410.41047 -33.53118,16.8606 -16.65699,-29.82359 z"/>
+						<path id="path4755" d="m 209.57067,408.9819 21.46882,18.28917 -16.65699,-29.82359 z"/>
+						<path id="path4757" d="m 191.71353,396.12476 21.46882,-15.99655 1.20015,17.31927 z"/>
+						<path id="path4776" d="m 191.71353,396.12476 21.46882,-15.99655 -42.37128,5.8907 z"/>
+						<path id="path4778" d="m 191.71353,396.12476 -12.10261,16.14631 -8.79985,-26.25216 z"/>
+						<path id="path4780" d="m 209.57067,408.9819 21.46882,18.28917 -32.37128,3.03355 z"/>
+						<path id="path4799" d="m 191.71353,396.12476 -12.10261,16.14631 12.62872,-1.25216 z"/>
+						<path id="path4801" d="m 215.99924,358.26761 33.61168,-68.1394 -54.51413,26.60498 z"/>
+						<path id="path4818" d="m 197.42781,276.83904 52.18311,13.28917 -54.51413,26.60498 z"/>
+						<path id="path4820" d="m 183.85639,428.9819 -4.24547,-16.71083 12.62872,-1.25216 z"/>
+						<path id="path4822" d="m 183.85639,428.9819 12.89739,1.14631 -4.51414,-19.1093 z"/>
+						<path id="path4824" d="m 209.57068,408.9819 -12.8169,21.14631 -4.51414,-19.1093 z"/>
+						<path id="path4858" d="m 183.85639,428.9819 12.89739,1.14631 4.77157,23.03356 z"/>
+						<path id="path4860" d="m 183.85639,428.9819 -4.24547,-16.71083 -30.22842,17.31927 z"/>
+						<path id="path4862" d="m 183.85639,428.9819 12.1831,38.28917 5.48586,-14.1093 z"/>
+						<path id="path4864" d="m 238.1421,468.26761 -42.10261,-0.99654 5.48586,-14.1093 z"/>
+						<path id="path4866" d="m 238.1421,468.26761 -42.10261,-0.99654 45.48586,16.60499 z"/>
+						<path id="path4878" d="m 183.85639,428.9819 -29.24547,18.64631 -5.22842,-18.03787 z"/>
+						<path id="path4880" d="m 121.35639,448.26761 33.25453,-0.6394 -5.22842,-18.03787 z"/>
+						<path id="path4961" d="m 121.35639,448.26761 33.25453,-0.6394 -27.72842,13.74784 z"/>
+						<path id="path5128" d="m 121.35639,448.26761 5.04024,13.64631 -30.942701,20.53356 z"/>
+						<path id="path5130" d="m 121.35639,448.26761 -41.031188,23.28917 15.128727,10.8907 z"/>
+						<path id="path5136" d="m 197.42781,276.83904 -24.95974,-1.53226 22.62872,41.42641 z"/>
+						<path id="path5138" d="m 197.42781,276.83904 -24.24546,-0.63941 -7.72842,-13.39501 z"/>
+						<path id="path5140" d="m 197.42781,276.83904 52.18311,13.28917 -28.08556,-49.10931 z"/>
+						<path id="path5142" d="m 197.42781,276.83904 -35.49547,-40.81797 59.59302,4.99783 z"/>
+						<path id="path5144" d="m 197.42781,276.83904 -35.31689,-40.81797 3.16444,27.14069 z"/>
+						<path id="path5166" d="m 197.42781,276.83904 -35.31689,-40.81797 3.16444,27.14069 z"/>
+						<path id="path5168" d="m 142.42781,278.9819 30.75454,-2.78227 -7.72842,-13.39501 z"/>
+						<path id="path5170" d="m 142.42781,278.9819 -12.81689,-7.78227 35.84301,-8.39501 z"/>
+						<path id="path5172" d="m 162.42781,235.41047 -32.81689,35.78916 35.84301,-8.39501 z"/>
+						<path id="path5174" d="m 183.1421,213.26761 -21.20976,22.75346 59.59302,4.99783 z"/>
+						<path id="path5176" d="m 183.1421,213.26761 34.50453,2.03917 3.87873,25.71212 z"/>
+						<path id="path5186" d="m 183.1421,213.26761 34.50453,2.03917 -9.6927,-23.57359 z"/>
+						<path id="path5188" d="m 183.1421,213.26761 9.50453,-30.81797 15.3073,9.28355 z"/>
+						<path id="path5190" d="m 183.1421,213.26761 9.50453,-30.81797 -22.54984,15.71212 z"/>
+						<path id="path5200" d="m 174.57067,179.69618 18.07596,2.75346 -22.54984,15.71212 z"/>
+						<path id="path5202" d="m 174.57067,179.69618 -14.0669,2.75346 9.59302,15.71212 z"/>
+						<path id="path5204" d="m 174.57067,179.69618 -14.0669,2.75346 -13.97841,-36.43074 z"/>
+						<path id="path5206" d="m 121.71353,176.83904 38.79024,5.6106 9.59302,15.71212 z"/>
+						<path id="path5208" d="m 121.71353,176.83904 38.79024,5.6106 -32.54984,-14.28788 z"/>
+						<path id="path5258" d="m 121.71353,176.83904 -26.924045,-34.3894 33.164445,25.71212 z"/>
+						<path id="path5260" d="m 238.85639,184.33904 -21.20976,30.96774 -9.6927,-23.57359 z"/>
+						<path id="path5262" d="m 234.57067,166.4819 -41.92404,15.96774 15.3073,9.28355 z"/>
+						<path id="path5270" d="m 234.92782,166.83904 -42.28119,15.6106 11.02159,-40.35931 z"/>
+						<path id="path5272" d="m 234.92782,166.83904 -7.63833,-23.3894 -23.62127,-1.35931 z"/>
+						<path id="path5286" d="m 234.92782,166.83904 -7.63833,-23.3894 12.8073,3.99783 z"/>
+						<path id="path5288" d="m 234.92782,166.83904 -7.63833,-23.3894 -23.62127,-1.35931 z"/>
+						<path id="path5296" d="m 234.92782,166.83904 18.07596,-16.24654 -12.90699,-3.14503 z"/>
+						<path id="path5298" d="m 278.49925,120.41047 -25.49547,30.18203 -12.90699,-3.14503 z"/>
+						<path id="path5306" d="m 234.92782,166.83904 -7.63833,-23.3894 -23.62127,-1.35931 z"/>
+						<path id="path5310" d="m 278.49925,120.41047 -9.42404,-6.60368 -28.97842,33.64068 z"/>
+						<path id="path5316" d="m 278.49925,120.41047 -9.42404,-6.60368 12.80729,-45.645034 z"/>
+						<path id="path5324" d="m 203.14211,141.4819 24.14738,1.96774 -13.26413,-12.0736 z"/>
+						<path id="path5341" d="m 233.14211,133.26761 -5.85262,10.18203 -13.26413,-12.0736 z"/>
+						<path id="path5343" d="m 203.14211,141.4819 -14.42405,-16.60369 25.3073,6.49783 z"/>
+						<path id="path5345" d="m 156.71354,88.267615 32.00452,36.610595 25.3073,6.49783 z"/>
+						<path id="path5357" d="m 233.14211,133.26761 -4.78119,-18.03226 -14.33556,16.14069 z"/>
+						<path id="path5359" d="m 233.14211,133.26761 -4.78119,-18.03226 12.45015,-6.00217 z"/>
+						<path id="path5361" d="m 220.64211,56.481896 7.71881,58.753454 12.45015,-6.00217 z"/>
+					</svg>
+				</div>
+
+				<script src="https://cdnjs.cloudflare.com/ajax/libs/animejs/2.0.0/anime.min.js"></script>
+				<script src="js/animate-logo.js" type="text/javascript"></script>
+				<div class="header__top__title">
+					<h2 id="animate-title">
+						Портфоліо
+					</h2>
+					<div class="shadow">
+					</div>
+				</div>
+				
+				<div class="tab">
+					<svg class="tab__svg" width="100%" height="400" viewBox="0 0 100 100" preserveAspectRatio="none" fill="#44A36F" version="1.1" xmlns="http://www.w3.org/2000/svg">
+						<path d="M 100 100 V 10 L 0 100"/>
+						<path d="M 30 73 L 100 18 V 10 Z" fill="#308355" stroke-width="0"/>
+					</svg>
+				</div>
+			</div>
+			<div class="header__mid wrapper">
+				<div class="showSite">
+					<div class="showSite__title">
+						<h4>
+							Мої роботи
+						</h2>
+						<div class="shadow">
+						</div>
+					</div>
+					<ul>
+						<li>
+							<a href="https://aborigen19961202.github.io/movus/" target="_blank"></a>
+						</li>
+						<li>
+							<a href="https://aborigen19961202.github.io/minecraft/" target="_blank"></a>
+						</li>
+						<li>
+							<a href="https://aborigen19961202.github.io/maki/" target="_blank"></a>
+						</li>
+						<li>
+							<a href="http://design.nltu.edu.ua" target="_blank"></a>
+						</li>
+						<li>
+							<a href="#"></a>
+						</li>
+					</ul>
+				</div>
+					
+			</div>
+			<div class="header__bot">
+				<div class="tab_turn">
+					<svg class="tab__svg" width="100%" height="400" viewBox="0 0 100 100" preserveAspectRatio="none" fill="#44A36F" version="1.1" xmlns="http://www.w3.org/2000/svg" transform="rotate(180)">
+						<path d="M 100 200 V 10 L 0 100"/>
+						<path d="M 30 73 L 100 18 V 10 Z" fill="#308355" stroke-width="0"/>
+					</svg>
+				</div>
+			</div>
+			
+		</div>
+		<div class="main">
+			<div class="about wrapper">
+				<div class="about__title">
+					Про мене
+					<div class="shadow">
+					</div>
+				</div>
+				<p>Навички:</p>
+				<div class="col-3"> 
+					<ul> 
+						<li>
+							HTML - впевнений користувач
+						</li>
+						<li>
+							CSS - впевнений користувач
+						</li>
+						<li>
+							SCSS(SASS) - впевнений користувач
+						</li>
+						<li>
+							PUG - впевнений користувач
+						</li>
+						<li>
+							UX/UI - експерт
+						</li>
+						<li>
+							Gulp - впевнений користувач
+						</li>
+					</ul>
+					<ul> 
+						<li>
+							JavaScript - середній рівень
+						</li>
+						<li>
+							GIT - середній рівень
+						</li>
+						<li>
+							Adaptive web design - середній рівень 
+						</li>
+						<li>
+							Grid Layout - середній рівень 
+						</li>
+						<li>
+							Photoshop - середній рівень
+						</li>
+						<li>
+							DataLife Engine - середній рівень
+						</li>
+					</ul>
+					<ul> 
+						<li>
+							PHP - початковий рівень
+						</li>
+						<li>
+							Бази даних MySQL - початковий рівень
+						</li>
+						<li>
+							jQuery - початковий рівень
+						</li>
+						<li>
+							Bootstrap 4 - початковий рівень
+						</li>
+						<li>
+							WordPress - початковий рівень
+						</li>
+						<li>
+							PixelPerfect верстка
+						</li>
+					</ul>
+				</div>
+				<p>Особисті якості:</p>
+				<div class="col-3"> 
+					<ul> 
+						<li>
+							Гумор
+						</li>
+						<li>
+							уважність до дрібниць, деталей, легкий перфекціонізм
+						</li>
+						<li>
+							відповідальніть до роботи
+						</li>
+						<li>
+							пунктуальність!
+						</li>
+					</ul>
+					<ul> 
+						<li>
+							ініціативність
+						</li>
+						<li>
+							бажання постійно вдосконалювати свої навички
+						</li>
+						<li>
+							вітаю конструктивну критику
+						</li>
+					</ul>
+					<ul> 
+						<li>
+							вміння писати красивий і зрозумілий код
+						</li>
+						<li>
+							відсутність поганих звичок
+						</li>
+						<li>
+							На курсах мене навчили дотримуватись логіки коли даю імена об'єктам
+						</li>
+					</ul>
+				</div>
+				<p>Освіта:</p>
+				<ul>
+					<li>
+						Дошкільна підготовка =) - літо 2003 року.
+						<p>"Бузьки і лелеки різні тварини", або <br>"<u>Вчителька:</u> Що ви чуєте зранку як тільки прокинулись ?  <br><u>Хтось:</u> як пташки співають, як листя шелестить<br><u>Я:</u> Як пси гавкают".</p>
+					</li>
+					<li>
+						Загально Освітня Школа - 2003р-2012р.
+						<p>- 1 клас - перша закоханість.</p>
+						<p>- 6 клас - перший комп'ютер за 50грн, пізнання азів, дискети, 2д ігри.</p>
+						<p>- 8 клас - перший нормальний комп'ютер за 5000 грн, 3д ігри, інтернет.</p>
+						<p>- 9 клас - майнкрафт, перший власний ігровий сервер на віртуалці який до того ж приносив прибуток, ЗАНУРЕННЯ під капот величезної машини ім'я якої програмування, перші спроби створити власний сайт, DLE, перша верстка )) в попу особисте життя, задротим )</p>
+					</li>
+					<li>
+						Вище Художнє Професійне Училище, професія: столяр, технолог, ДО верстатник 5 розряду - 2012р-2017р.
+						<p>- 3 курс - получив права в автошколі, категорія B </p>
+						<p>- 4 курс - становлення нової філософії, кардинальні зміни світосприйняття, зародження любові до наук, прийняття атеїзму, засудження метафізики.</p>
+					</li>
+					<li>
+						Circle Web Group, Front-end який косить під фулстак - 2017р. (6 міс.).
+					</li>
+					<li>
+						НЛТУ (Дизайн)- 2017р-2019р.
+						<p>Остаточне розчарування в держваній освіті, ПОКИ САМ НЕ ПОЧНЕШ ВЧИТИ, НЕ НАВЧИШСЯ! хвала ютюбу з гуглом</p>
+					</li>
+				</ul>
+				<p>Недостатки:</p>
+				<ul>
+					<li>
+						Щоб досягти моєї максимальної ефективності, мені потрібен гнучкий графік або іноді навіть і віддалена робота, буває що велике бажання кодити приходить під вечір або ж вночі і десь в горах :D
+					</li>
+					<li>
+						Пізно взявся за голову.
+					</li>
+				</ul>
+				<p>Чому я ?:</p>
+				<ul>
+					<p>
+						Я планую зв'язати своє життя з ІТ, планую багато вчитись і бо мені це подобається.
+					</p>
+				</ul>
+			</div>
+
+			<div class="main__top">
+				
+				<div class="tab">
+					<svg class="tab__svg" width="100%" height="400" viewBox="0 0 100 100" preserveAspectRatio="none" fill="#44A36F" version="1.1" xmlns="http://www.w3.org/2000/svg">
+						<path d="M 0 100 V 10 L 100 100"/>
+						<path d="M 70 73 L 0 18 V 10 Z" fill="#308355" stroke-width="0"/>
+					</svg>
+				</div>
+			</div>
+			<div class="main__mid">
+				<ul class="socialIcons col-6">
+					<li><a href="tel:380999075965" target="_blank" class="phone"><i class="fa fa-phone"></i></a></li>
+					<li><a href="mailto:aborigen19961202@gmail.com?subject=Пропозиція роботи" target="_blank" class="email"><i class="fa fa-at"></i></a></li>
+					<li><a href="viber://chat?number=+380999075965" target="_blank" class="viber"><i class="fab fa-viber"></i></a></li>
+					<li><a href="skype:aborigen19961202?chat" target="_blank" class="skype"><i class="fab fa-skype"></i></a></li>
+					<li><a href="https://www.facebook.com/andry.kricun" target="_blank" class="facebook"><i class="fa fa-facebook "></i></a></li>
+					<li><a href="https://vk.com/idlritsunandry" target="_blank" class="vk"><i class="fa fa-vk"></i></a></li>
+					<li><a href="https://twitter.com/aborigen1996120" target="_blank" class="twitter"><i class="fa fa-twitter"></i></a></li>
+					<li><a href="https://www.instagram.com/aborigen19961202/" target="_blank" class="instagram"><i class="fa fa-instagram"></i></a></li>
+					<li><a href="https://plus.google.com/u/0/105841895943681325271" target="_blank" class="googlePlus"><i class="fa fa-google-plus"></i></a></li>
+					<li><a href="https://www.youtube.com/channel/UC1zfCKGRqFu1mABGmtRB_4w?view_as=subscriber" target="_blank" class="youtube"><i class="fa fa-youtube"></i></a></li>
+					<li><a href="https://codepen.io/aborigen19961202" target="_blank" class="codepen"><i class="fa fa-codepen"></i></a></li>
+					<li><a href="tg://resolve?domain=aborigen19961202" target="_blank" class="telegram"><i class="fab fa-telegram-plane"></i></a></li>
+					<li><a href="https://steamcommunity.com/profiles/76561198124367539/" target="_blank" class="steam"><i class="fab fa-steam"></i></a></li>
+					<li><a href="https://linkedin.com/in/aborigen19961202" target="_blank" class="linkedin"><i class="fab fa-linkedin"></i></a></li>
+				</ul>
+			</div>
+			<div class="main__bot">
+				<div class="tab_turn">
+					<svg class="tab__svg" width="100%" height="400" viewBox="0 0 100 100" preserveAspectRatio="none" fill="#44A36F" version="1.1" xmlns="http://www.w3.org/2000/svg" transform="rotate(180)">
+						<path d="M 0 100 V 10 L 100 100"/>
+						<path d="M 70 73 L 0 18 V 10 Z" fill="#308355" stroke-width="0"/>
+					</svg>
+				</div>
+			</div>
+			
+		</div>
+	</body>
+</html>
